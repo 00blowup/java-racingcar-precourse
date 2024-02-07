959ba751972abe66d23a00240305b8d2414b6faa
Haeun Noh <00blowup@gmail.com>
2024-02-07 17:39:34 +0900

    feat: Car 내부 메소드 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,31 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
+    // 필드
     private final String name;
     private int position = 0;
 
+    // 생성자
     public Car(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public int getPosition() {
+        return position;
+    }
+
+    // 전진 여부를 결정한 뒤 위치 값을 갱신하는 메소드
+    public void goOrStop() {
+        if(Randoms.pickNumberInRange(0,9)>=4) position++;
+    }
+
+    // 본인의 현재 위치를 출력하는 메소드
+    public void printMyself() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        sb.append(" : ");
+        for(int i=0; i<position; i++) sb.append("-");
+        System.out.println(sb);
+    }
 }
